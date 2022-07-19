89e004add249746153721c6b6b6fd5099bdfdc9b
Evan Grantham-Brown <malegrim@gmail.com>
2022-07-19 14:05:17 -0400

    Cleaning up the test suite for dependencies.

--- tests/dependencies.spec.ts
+++ tests/dependencies.spec.ts
@@ -1,152 +1,7 @@
 import { MixinDependGraph, MixinDependNode } from '../src/dependencies';
 import { CircularDependencyError } from '../src/errors';
 
-let GraphClass: any;
-let NodeClass: any;
-
 describe('MixinDependGraph', () => {
-  it('correctly orders a list of dependencies', () => {
-    const NodeClass = MixinDependNode(class {});
-    const GraphClass = MixinDependGraph(NodeClass, class {});
-
-    const graph = new GraphClass();
-
-    const node1 = new NodeClass();
-    graph.addDependNode(node1);
-    const node2 = new NodeClass();
-    graph.addDependNode(node2);
-    const node3 = new NodeClass();
-    graph.addDependNode(node3);
-    const node4 = new NodeClass();
-    graph.addDependNode(node4);
-    const node5 = new NodeClass();
-    graph.addDependNode(node5);
-
-    node1.addDepend(node2);
-    node1.addDepend(node4);
-
-    node2.addDepend(node3);
-
-    node5.addDepend(node3);
-
-    // This must be executed before dependsInEvalOrder().
-    graph.findDependCycle();
-
-    // our graph in crude ASCII art:
-    //
-    //    |------>(node4)
-    //    |
-    // (node1)--->(node2)--->(node3)
-    //                    |
-    //                    |
-    //            (node5)-|
-
-    // So we expect nodes 3 and 4, as the leaf nodes, to evaluate first.
-    // Then nodes 2 and 5 are ready; they evaluate next.
-    // Finally node 1.
-
-    // We also expect that where two nodes are ready to evaluate at the
-    // same time, the initial ordering is preserved (so 3 evaluates before
-    // 4).
-    const depends = graph.dependsInEvalOrder();
-    expect(depends).toEqual([node3, node4, node2, node5, node1]);
-  });
-
-  it('requires findDependCycle() to run before dependsInEvalOrder()', () => {
-    const NodeClass = MixinDependNode(class {});
-    const GraphClass = MixinDependGraph(NodeClass, class {});
-
-    const graph = new GraphClass();
-
-    const node1 = new NodeClass();
-    graph.addDependNode(node1);
-    const node2 = new NodeClass();
-    graph.addDependNode(node2);
-    const node3 = new NodeClass();
-    graph.addDependNode(node3);
-    const node4 = new NodeClass();
-    graph.addDependNode(node4);
-    const node5 = new NodeClass();
-    graph.addDependNode(node5);
-
-    node1.addDepend(node2);
-    node1.addDepend(node4);
-
-    node2.addDepend(node3);
-
-    node5.addDepend(node3);
-
-    expect(() => {
-      graph.dependsInEvalOrder();
-    }).toThrow(CircularDependencyError);
-  });
-
-  it('errors if dependsInEvalOrder() is run on a cyclic graph', () => {
-    const NodeClass = MixinDependNode(class {});
-    const GraphClass = MixinDependGraph(NodeClass, class {});
-
-    const graph = new GraphClass();
-
-    const node1 = new NodeClass();
-    graph.addDependNode(node1);
-    const node2 = new NodeClass();
-    graph.addDependNode(node2);
-    const node3 = new NodeClass();
-    graph.addDependNode(node3);
-    const node4 = new NodeClass();
-    graph.addDependNode(node4);
-    const node5 = new NodeClass();
-    graph.addDependNode(node5);
-
-    node1.addDepend(node2);
-    node1.addDepend(node4);
-
-    node2.addDepend(node3);
-
-    node5.addDepend(node3);
-
-    // oops, we made a cycle
-    node3.addDepend(node1);
-
-    expect(() => {
-      graph.findDependCycle();
-      graph.dependsInEvalOrder();
-    }).toThrow(CircularDependencyError);
-  });
-
-  it('detects and returns a cycle with findDependCycle()', () => {
-    const NodeClass = MixinDependNode(class {});
-    const GraphClass = MixinDependGraph(NodeClass, class {});
-
-    const graph = new GraphClass();
-
-    const node1 = new NodeClass();
-    graph.addDependNode(node1);
-    const node2 = new NodeClass();
-    graph.addDependNode(node2);
-    const node3 = new NodeClass();
-    graph.addDependNode(node3);
-    const node4 = new NodeClass();
-    graph.addDependNode(node4);
-    const node5 = new NodeClass();
-    graph.addDependNode(node5);
-
-    node1.addDepend(node2);
-    node2.addDepend(node3);
-    node3.addDepend(node4);
-    node4.addDepend(node2);
-
-    const cycle = graph.findDependCycle();
-
-    expect(cycle).toContain(node2);
-    expect(cycle).toContain(node3);
-    expect(cycle).toContain(node4);
-    expect(cycle).not.toContain(node1);
-
-    expect(graph.dependsAcyclic).toBe(false);
-    expect(graph.dependsCyclic).toBe(true);
-  });
-
   it('works properly with other class properties and functions', () => {
     const NodeClass = MixinDependNode(class {
       public foo() {
@@ -167,5 +22,145 @@ describe('MixinDependGraph', () => {
 
     const graphInst = new GraphClass(13);
     expect(graphInst.bar).toEqual(13);
+  });
+});
+
+describe('MixinDependGraph.dependsInEvalOrder()', () => {
+  const NodeClass = MixinDependNode(class {});
+  const GraphClass = MixinDependGraph(NodeClass, class {});
+  let graph: GraphClass;
+  let node1, node2, node3, node4, node5: NodeClass;
+
+  beforeEach(() => {
+    graph = new GraphClass();
+
+    node1 = new NodeClass();
+    graph.addDependNode(node1);
+    node2 = new NodeClass();
+    graph.addDependNode(node2);
+    node3 = new NodeClass();
+    graph.addDependNode(node3);
+    node4 = new NodeClass();
+    graph.addDependNode(node4);
+    node5 = new NodeClass();
+    graph.addDependNode(node5);
+  });
+
+  it('correctly orders a list of dependencies', () => {
+    node1.addDepend(node2);
+    node1.addDepend(node4);
+    node2.addDepend(node3);
+    node5.addDepend(node3);
+
+    // Our graph in crude ASCII art:
+    //
+    //    |------>(node4)
+    //    |
+    // (node1)--->(node2)--->(node3)
+    //                    |
+    //                    |
+    //            (node5)-|
+
+    // So we expect nodes 3 and 4, as the leaf nodes, to evaluate first.
+    // Then nodes 2 and 5 are ready; they evaluate next.
+    // Finally node 1.
+
+    // We also expect that where two nodes are ready to evaluate at the
+    // same time, the initial ordering is preserved (so 3 evaluates before
+    // 4).
+    graph.findDependCycle();
+    const depends = graph.dependsInEvalOrder();
+    expect(depends).toEqual([node3, node4, node2, node5, node1]);
+  });
+
+  it('requires findDependCycle() to run first', () => {
+    node1.addDepend(node2);
+    node1.addDepend(node4);
+    node2.addDepend(node3);
+    node5.addDepend(node3);
+
+    expect(() => {
+      graph.dependsInEvalOrder();
+    }).toThrow(CircularDependencyError);
+  });
+
+  it('errors if run on a cyclic graph', () => {
+    node1.addDepend(node2);
+    node1.addDepend(node4);
+    node2.addDepend(node3);
+    node5.addDepend(node3);
+    node3.addDepend(node1); // oops, we made a cycle
+
+    expect(() => {
+      graph.findDependCycle();
+      graph.dependsInEvalOrder();
+    }).toThrow(CircularDependencyError);
+  });
+});
+
+describe('MixinDependGraph.findDependCycle()', () => {
+  const NodeClass = MixinDependNode(class {});
+  const GraphClass = MixinDependGraph(NodeClass, class {});
+  let graph: GraphClass;
+  let node1, node2, node3, node4: NodeClass;
+
+  beforeEach(() => {
+    graph = new GraphClass();
+
+    node1 = new NodeClass();
+    graph.addDependNode(node1);
+    node2 = new NodeClass();
+    graph.addDependNode(node2);
+    node3 = new NodeClass();
+    graph.addDependNode(node3);
+    node4 = new NodeClass();
+    graph.addDependNode(node4);
+  });
+
+  it('detects and returns a cycle', () => {
+    node1.addDepend(node2);
+    node2.addDepend(node3);
+    node3.addDepend(node4);
+    node4.addDepend(node2);
+
+    const cycle = graph.findDependCycle();
+
+    expect(cycle).toContain(node2);
+    expect(cycle).toContain(node3);
+    expect(cycle).toContain(node4);
+    expect(cycle).not.toContain(node1);
+  });
+
+  it('sets the cyclic, acyclic flags to true, false when a cycle is present', () => {
+    node1.addDepend(node2);
+    node2.addDepend(node3);
+    node3.addDepend(node4);
+    node4.addDepend(node2);
+
+    graph.findDependCycle();
+
+    expect(graph.dependsCyclic).toBe(true);
+    expect(graph.dependsAcyclic).toBe(false);
+  });
+
+  it('returns null if no cycle is present', () => {
+    node1.addDepend(node2);
+    node2.addDepend(node3);
+    node3.addDepend(node4);
+
+    const cycle = graph.findDependCycle();
+
+    expect(cycle).toBe(null);
+  });
+
+  it('sets the cyclic, acyclic flags to false, true when no cycle is present', () => {
+    node1.addDepend(node2);
+    node2.addDepend(node3);
+    node3.addDepend(node4);
+
+    graph.findDependCycle();
+
+    expect(graph.dependsCyclic).toBe(false);
+    expect(graph.dependsAcyclic).toBe(true);
   });
 });
